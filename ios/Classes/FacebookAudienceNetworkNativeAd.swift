842d043af799848efa4be112d54e5d7af5e5da6c
Jang Taehwan <tata8664@gmail.com>
2021-06-09 11:28:04 +0900

    fix reversed ad invalidated status on ios

--- ios/Classes/FacebookAudienceNetworkNativeAd.swift
+++ ios/Classes/FacebookAudienceNetworkNativeAd.swift
@@ -568,7 +568,7 @@ class FacebookAudienceNetworkNativeAdView: NSObject, FlutterPlatformView, FBNati
         regNativeAdView()
 
         let placement_id: String = nativeAd.placementID
-        let invalidated: Bool = nativeAd.isAdValid
+        let invalidated: Bool = !nativeAd.isAdValid
         let arg: [String: Any] = [
             FANConstant.PLACEMENT_ID_ARG: placement_id,
             FANConstant.INVALIDATED_ARG: invalidated,
@@ -595,7 +595,7 @@ class FacebookAudienceNetworkNativeAdView: NSObject, FlutterPlatformView, FBNati
         print("NativeAd > nativeAdWillLogImpression")
 
         let placement_id: String = nativeAd.placementID
-        let invalidated: Bool = nativeAd.isAdValid
+        let invalidated: Bool = !nativeAd.isAdValid
         let arg: [String: Any] = [
             FANConstant.PLACEMENT_ID_ARG: placement_id,
             FANConstant.INVALIDATED_ARG: invalidated,
@@ -608,7 +608,7 @@ class FacebookAudienceNetworkNativeAdView: NSObject, FlutterPlatformView, FBNati
         print("NativeAd > nativeAd %s", error)
 
         let placement_id: String = nativeAd.placementID
-        let invalidated: Bool = nativeAd.isAdValid
+        let invalidated: Bool = !nativeAd.isAdValid
         let errorStr: String = error as? String ?? "";
         let arg: [String: Any] = [
             FANConstant.PLACEMENT_ID_ARG: placement_id,
@@ -623,7 +623,7 @@ class FacebookAudienceNetworkNativeAdView: NSObject, FlutterPlatformView, FBNati
         print("NativeAd > nativeAdDidClick")
         
         let placement_id: String = nativeAd.placementID
-        let invalidated: Bool = nativeAd.isAdValid
+        let invalidated: Bool = !nativeAd.isAdValid
         let arg: [String: Any] = [
             FANConstant.PLACEMENT_ID_ARG: placement_id,
             FANConstant.INVALIDATED_ARG: invalidated,
